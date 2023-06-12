3aa7681d40356498aef026e82ccf0e56479a197f
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-11 22:25:37 -0700

    📝 login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider>
+        <Box
+          w="100%"
+          h="100vh"
+          bgImage="linear-gradient(to-br, #a551ff , #46e0e3f8)"
+        >
+          <Component {...pageProps} />
+        </Box>
+      </ChakraProvider>
+    </>
+  );
 }
