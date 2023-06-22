96150ed98cb13d928adc36cb643af97d1e9052e3
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-21 19:31:31 -0700

    🚧 Difficult

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import "@/styles/globals.css";
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import { auth } from "./firebase";
+
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(auth);
   return (
     <>
       <ChakraProvider>
