68f77a62cc97cde93af969e9f8a3cf57a85bea36
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-23 16:12:36 -0700

    ⚡️ Finished Sign Up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,10 @@
 import "@/styles/globals.css";
-import { Box, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-
-import { auth } from "./firebase";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ChakraProvider>
-        <Box
-          w="100%"
-          h="100vh"
-          bgImage="linear-gradient(to-br, #a551ff , #46e0e3f8)"
-        >
-          <Component {...pageProps} />
-        </Box>
-      </ChakraProvider>
+      <Component {...pageProps} />
     </>
   );
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,92 +1,178 @@
-import React, { Fragment, useState } from "react"
-  Box,
-  Button,
-  Container,
-  FormControl,
-  FormLabel,
-  Heading,
-  HStack,
-  Input,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import { Logo } from "@/components/Logo";
-// import { OAuthButtonGroup } from "@/components/OAuthButtonGroup";
-import { PasswordField } from "@/components/PasswordField"; 
-
-// signup
-import { auth } from "./firebase";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./firebase";
 
-export const Signup: React.FC<{}> = () => {
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-  // 新規登録で使用するemailとpasswordの状態管理
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string | number>("");
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
 
-  // 入力された値をuseStateで管理するようの関数
-  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.currentTarget.value)
-  }
+// export default function SignUp()
+const SignUp = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.currentTarget.value)
-  }
-    
+  // emailとpasswordの状態管理
+  // useStateにReactが必要な理由？
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const Register = (e: any) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        alert(error.message);
+        console.log(error);
+      });
+  };
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
 
   return (
-    <>
-      <Container
-        pos="absolute"
-        top="120"
-        left="280"
-        bg="#ffc2c2"
-        maxW="lg"
-        py={{ base: "12", md: "24" }}
-        px={{ base: "0", sm: "8" }}
-      >
-        <Stack spacing="8">
-          <Stack spacing="6">
-            <Logo />
-            <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
-              <Heading size={{ base: "xs", md: "sm" }}>
-                Create an account
-              </Heading>
-              <HStack spacing="1" justify="center">
-                <Text color="fg.muted">Already have an account?</Text>
-                <Button variant="text" colorScheme="blue">
-                  Login
-                </Button>
-              </HStack>
-            </Stack>
-          </Stack>
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
-            py={{ base: "0", sm: "8" }}
-            px={{ base: "4", sm: "10" }}
-            bg={{ base: "#ecd0d0", sm: "bg.surface" }}
-            boxShadow={{ base: "none", sm: "md" }}
-            borderRadius={{ base: "none", sm: "xl" }}
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <Stack spacing="6">
-              <Stack spacing="5">
-                <FormControl>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input id="email" type="email" onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    handleChangeEmail(event)
-                  }}/>
-                </FormControl>
-                <PasswordField />
-              </Stack>
-
-              <Stack spacing="6">
-                <Button variant="primary">Create an account</Button>
-              </Stack>
-            </Stack>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              {/* <LockOutlinedIcon /> */}
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  handleChangeEmail(e);
+                }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  handleChangePassword(e);
+                }}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={Register}
+              >
+                Sign Up
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Already have an account? Sign In"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
           </Box>
-        </Stack>
-      </Container>
-    </>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
-export default Signup;
+export default SignUp;
