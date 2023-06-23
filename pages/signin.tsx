f39512633701263e14a186ee89946580ef0e235d
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-23 16:48:38 -0700

    added

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -126,9 +126,9 @@ const SignIn = () => {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  < href="#" variant="body2">
                     {"Don't have an account? Sign Up"}
-                  </Link>
+                  
                 </Grid>
               </Grid>
               <Copyright sx={{ mt: 5 }} />
