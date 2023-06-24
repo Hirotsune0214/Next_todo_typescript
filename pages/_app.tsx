03a3f95599ca9812dfcbb15b1ecb432fccdfb9f4
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-24 12:57:06 -0700

    ⚡️ Added bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import "@/styles/globals.css";
+import { Box } from "@mui/material";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Box
-          w="100%"
-          h="100vh"
-          bgImage="linear-gradient(to-br, #a551ff , #46e0e3f8)"
-        > */}
-      <Component {...pageProps} />
+      <Box
+        sx={{
+          background: "linear-gradient(to right bottom, #a551ff , #46e0e3f8)",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <Component {...pageProps} />
+      </Box>
     </>
   );
 }
