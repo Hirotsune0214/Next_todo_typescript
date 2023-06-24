2a6f12af463b7fbc2e8bb2541761ebcaded4f1d1
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-23 19:32:48 -0700

    💄  Added

---
+++ pages/add.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+
+export default function Add() {
+  return (
+    <>
+      <Box
+        sx={{
+          width: 650,
+          height: 610,
+          backgroundColor: "#ffffff",
+          border: "solid 5px #faf0b6",
+          borderRadius: "5%",
+          position: "absolute",
+          left: "25%",
+          top: "9%",
+        }}
+      />
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello world</h1>
+      <h1>Add_modal</h1>
     </>
   );
 }
