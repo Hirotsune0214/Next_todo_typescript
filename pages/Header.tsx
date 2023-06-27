240d08f1fe198e6153e62d5cae4d8859c9045ab6
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-06-26 18:45:23 -0700

    ⚡️ Added Link and Sign Out

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from "./Header.module.css";
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
+import { getAuth, signOut } from "firebase/auth";
+import Link from "next/link";
 
 const Header = () => {
+  // Logout
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        alert("LogOut is successful");
+        setTimeout(function () {
+          // TODO: merge後にリンク先を指定する
+          // <Link href="#"></Link>;
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <h1 className={styles.header}>Todo-List</h1>
@@ -14,6 +31,7 @@ const Header = () => {
             // 書き方を聞く
             // position: "absolute left-50px",
           }}
+          onClick={handleSignOut}
         />
         <p className={styles.headerLetter}>Log Out</p>
       </div>
